ddcfd438c1cee17014f6335869b7ac4f0920100e
PERSONAL <PERSONAL@192.168.56.1>
2020-05-31 18:45:38 +0700

    Uploud project

---
+++ src/E31191124_queue/Runner.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package E31191124_queue;
+
+/**
+ *
+ * @author Khusnan
+ */
+public class Runner {
+
+	public static void main(String[] args) {
+		E31191124_Queue q = new E31191124_Queue();
+		q.deQueue();
+
+		q.enQueue(5); //menambahkan elemen 5 ke dalam queue q
+		q.enQueue(1); //menambahkan elemen 1 ke dalam queue q
+		q.enQueue(0); //menambahkan elemen 0 ke dalam queue q
+		q.enQueue(2); //menambahkan elemen 2 ke dalam queue q
+
+		q.deQueue();
+
+		q.enQueue(4); //menambahkan elemen 4 ke dalam queue q
+		q.enQueue(2); //menambahkan elemen 2 ke dalam queue q
+
+		q.enQueue(6); //menambahkan elemen 6 ke dalam queue q
+
+		System.out.println(q.isFull());
+
+		System.out.println("Size " + q.getSize());
+		q.show();
+	}
+}
